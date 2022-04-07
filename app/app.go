a41725a8b0295a0d2fa7f4988683071582b03269
Wiliender <wiliendersilva@gmail.com>
2022-04-07 18:35:34 -0300

    creating an IP lookup functionality, using the net package

--- app/app.go
+++ app/app.go
@@ -1,6 +1,9 @@
 package app
 
 import (
+	"log"
+	"net"
+
 	"github.com/urfave/cli"
 )
 
@@ -10,5 +13,34 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na internet"
 
+	app.Commands = []cli.Command{ //vai ser um slice de comandos que a aplicação vai entender e conseguir executar
+		{
+			Name:  "ip", //comando que busca os ips
+			Usage: "Busca IPs de endereços na internet",
+			Flags: []cli.Flag{ //flag que vai receber os valores para o comando funcionar
+				cli.StringFlag{ //parametros da flag
+					Name:  "host",
+					Value: "amazon.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	//NET
+	ips, erro := net.LookupHost(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		log.Println(ip)
+	}
+
 }
