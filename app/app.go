81b8cead62271ccaa26928188e9517fe682bd8bf
Wiliender <wiliendersilva@gmail.com>
2022-04-07 23:34:28 -0300

    Creating a Public Servers lookup functionality, and build project

--- app/app.go
+++ app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net"
 
@@ -13,17 +14,25 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na internet"
 
+	flags := []cli.Flag{ //flag que vai receber os valores para o comando funcionar
+		cli.StringFlag{ //parametros da flag
+			Name:  "host",
+			Value: "amazon.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{ //vai ser um slice de comandos que a aplicação vai entender e conseguir executar
 		{
-			Name:  "ip", //comando que busca os ips
-			Usage: "Busca IPs de endereços na internet",
-			Flags: []cli.Flag{ //flag que vai receber os valores para o comando funcionar
-				cli.StringFlag{ //parametros da flag
-					Name:  "host",
-					Value: "amazon.com.br",
-				},
-			},
+			Name:   "ip", //comando que busca os ips
+			Usage:  "Busca IPs de endereços na internet",
+			Flags:  flags,
 			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca nomes de servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
 		},
 	}
 
@@ -33,14 +42,28 @@ func Gerar() *cli.App {
 func buscarIps(c *cli.Context) {
 	host := c.String("host")
 
-	//NET
+	//PACKAGE NET
 	ips, erro := net.LookupHost(host)
 	if erro != nil {
 		log.Fatal(erro)
 	}
 
 	for _, ip := range ips {
-		log.Println(ip)
+		fmt.Println(ip)
 	}
 
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	//PACKAGE NET
+	servidores, erro := net.LookupNS(host) //busca os nomes de servidores
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
+	}
 }
